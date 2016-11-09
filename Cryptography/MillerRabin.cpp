259beb6c8de843f7154767bee68f2b6029aeea4e
Abhijeet Talaulikar <abhijeetstalaulikar@gmail.com>
2016-11-09 20:16:17 +0530

    Update MillerRabin.cpp

--- Cryptography/MillerRabin.cpp
+++ Cryptography/MillerRabin.cpp
@@ -21,23 +21,6 @@ long int ModExp(long int x, unsigned long int y, int p) {
 	return res;
 }
 
-int jacobi(int a, int n) {
-	if(a == 0 || a == 1) return a;
-	int s, a1 = a, n1, e = 0;
-	while(a1 % 2 == 0) {
-		a1 /= 2;
-		e++;
-	}
-	if(e % 2) {
-		if(n % 8 == 1 || n % 8 == 7) s = 1;
-		else if(n % 8 == 3 || n % 8 == 5) s = -1;
-	} else s = 1;
-	if(n % 4 == 3 && a1 % 4 == 3) s *= -1;
-	n1 = n % a1;
-	if(a1 == 1) return s;
-	else return s * jacobi(n1, a1);
-}
-
 int MillerRabin(int n, int t) {
 	int i = 0, j;
 	int N = n-1;
@@ -78,5 +61,5 @@ int main() {
 	cin>>n;
 	cout<<"Enter security parameter: ";
 	cin>>t;
-	cout<<n<<" is "<<(SolovayStrassen(n, t) ? "prime" : "not prime")<<endl;
+	cout<<n<<" is "<<(MillerRabin(n, t) ? "prime" : "not prime")<<endl;
 }
