bd3b37387728571d7e8d8e74d9169b72d04aa0aa
Abhijeet Talaulikar <abhijeetstalaulikar@gmail.com>
2016-11-09 19:00:08 +0530

    Update MillerRabin.cpp

--- Cryptography/MillerRabin.cpp
+++ Cryptography/MillerRabin.cpp
@@ -10,21 +10,15 @@ int getRand(int max) {
 	return (rand() % range + 2);
 }
 
-int MExp(int a, int K, int n) {
-	int k[100], b, i = -1;
-	b = 1;
-	if(!K) return b;
-	while(K) {
-		if(K%2) k[++i] = 1;
-		else k[++i] = 0;
-		K /= 2;
+long int MExp(long int x, unsigned long int y, int p) {
+	long int res = 1;
+	x = x % p;
+	while (y > 0) {
+		if (y & 1) res = (res*x) % p;
+		y = y>>1;
+		x = (x*x) % p;
 	}
-	if(k[0]) b = a;
-	for(int j=1,A=a;j<=i;j++) {
-		A = (A*A) % n;
-		if(k[j]) b = (A * b) % n;
-	}
-	return b;
+	return res;
 }
 
 int MillerRabin(int n, int t) {
